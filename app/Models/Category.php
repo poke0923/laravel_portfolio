edbbd71eddab8d09bc6bec1cb7a6c3a5df097d3e
poke0923 <orbis.9482@gmail.com>
2023-12-11 11:44:41 +0900

    feat:#17カテゴリー毎の画面実装

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -14,4 +14,23 @@ class Category extends Model
     public function posts(){
         return $this->hasMany(Post::class);
     }
+    
+    public function getCategoryPaginate($limit=3){
+        return $this->posts()->with('category')->orderBy('updated_at','desc')->paginate($limit);
+        //基本的な形は$this->posts()->paginate();
+        //with('category')はEagerロードと呼ばれ、データベースへのアクセスを1回で済ませてくれる手法
+        /*
+        リレーション先のデータを取り出すコードの書き方の手順は
+        1. 基本的な形を作る　$this->posts()->paginate($limit);　※limitのところは数字でOK
+           posts()を呼び出すとPostクラスにつながるから、そこからペジネートしてデータを取得する
+           この時、どのcategory_idのデータを取り出すかは
+           CategoryContorollerでインスタンス化する$categoryにルーティングの時点で暗黙の結合により引き継がれている
+        2. orderByで取り出す順番を指定　$this->posts()->orderBy(~)->paginate();
+        3. with('category')をposts()の後に入れて、categoryからまとめてデータを取得して、それをorderByにより並べ替える。
+           こうすることでデータの取得を一度で済ませることができる。
+           $this->posts()->with('category')->orderBy('updated_at','desc')->paginate($limit);
+        */
+    }
+    
+
 }
