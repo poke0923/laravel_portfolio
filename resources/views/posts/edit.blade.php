30537709171cf60cb1926cd51e4335d2a34ab4f0
poke0923 <orbis.9482@gmail.com>
2023-12-28 02:19:06 +0900

    fix#37

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -12,6 +12,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
         
         <style>
             .error{
@@ -62,7 +63,7 @@
                             @foreach($tags as $tag)
                                 <input type="checkbox" name="tag[]" value="{{$tag->id}}" {{ in_array($tag->id, $post->tags->pluck('id')->toArray()) ? 'checked' : '' }}><label>{{$tag->name}}</label>
                                 <!--
-                                {{in_array()?'checked':''}}これの?以降で場合分け。trueならchecked、falseなら''を返す
+                                in_array()?'checked':''これの?以降で場合分け。trueならchecked、falseなら''を返す
                                 in_array(探したい値, 配列)で配列の中に探したい値があればtrue、なければfalseを返す
                                 やりたいことは「このタグのidってこのポストが持つタグのidと一致してますか？」ということなので
                                 1.探したい値にこのタグのid、配列にこの投稿の持つタグのidを持ってくる
@@ -73,6 +74,16 @@
                                 -->
                             @endforeach
                             
+                            <h2>撮影場所</h2>
+                            <input
+                                id="pac-input"
+                                class="controls"
+                                type="text"
+                                placeholder="Search Box"
+                              />
+                            <div id="map" style="height:500px; width:800px;"></div>
+                            <div>lat: <input id="lat" name="post[latitude]" type="text" value="{{$post->latitude}}"/></div>
+                            <div>lng: <input id="lng" name="post[longitude]" type="text" value="{{$post->longitude}}"/></div>
                             </br>
                             <x-primary-button class="mt-3">
                                 {{ __('保存') }}
@@ -85,7 +96,97 @@
                 </div>
             </div>
         </div>
-        
+        <script src="https://maps.googleapis.com/maps/api/js?key=APIKEY&callback=initAutocomplete&libraries=places&v=weekly"defer></script>
+        <script>
+          function initAutocomplete($post) {
+            const map = new google.maps.Map(document.getElementById("map"), {
+              center: { lat: {{$post->latitude}}, lng: {{$post->longitude}} },
+              zoom: 13,
+              mapTypeId: "roadmap",
+            });
+            
+            new google.maps.Marker({
+                position:  { lat: {{$post->latitude}}, lng: {{$post->longitude}} },
+                map,
+                title: "location",
+              }); 
+            
+            
+            // Create the search box and link it to the UI element.
+            const input = document.getElementById("pac-input");
+            const searchBox = new google.maps.places.SearchBox(input);
+            
+          
+            map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+            // Bias the SearchBox results towards current map's viewport.
+            map.addListener("bounds_changed", () => {
+              searchBox.setBounds(map.getBounds());
+            });
+          
+            let markers = [];
+          
+            // Listen for the event fired when the user selects a prediction and retrieve
+            // more details for that place.
+            searchBox.addListener("places_changed", () => {
+              const places = searchBox.getPlaces();
+          
+              if (places.length == 0) {
+                return;
+              }
+          
+              // Clear out the old markers.
+              markers.forEach((marker) => {
+                marker.setMap(null);
+              });
+              markers = [];
+          
+              // For each place, get the icon, name and location.
+              const bounds = new google.maps.LatLngBounds();
+          
+              places.forEach((place) => {
+                if (!place.geometry || !place.geometry.location) {
+                  console.log("Returned place contains no geometry");
+                  return;
+                }
+          
+                const icon = {
+                  url: place.icon,
+                  size: new google.maps.Size(71, 71),
+                  origin: new google.maps.Point(0, 0),
+                  anchor: new google.maps.Point(17, 34),
+                  scaledSize: new google.maps.Size(25, 25),
+                };
+          
+                // Create a marker for each place.
+                markers.push(
+                  new google.maps.Marker({
+                    map,
+                    icon,
+                    title: place.name,
+                    position: place.geometry.location,
+                  }),
+                );
+                
+                if (place.geometry.viewport) {
+                  
+                  // Only geocodes have viewport.
+                  bounds.union(place.geometry.viewport);
+                } else {
+                  console.log(place.geometry.location);
+                  bounds.extend(place.geometry.location);
+                }
+                console.log(place.geometry.location);
+                document.getElementById("lat").value = place.geometry.location.lat();
+                document.getElementById("lng").value = place.geometry.location.lng();
+                
+              });
+              
+              map.fitBounds(bounds);
+            });
+          }
+          
+          window.initAutocomplete = initAutocomplete;
+        </script>
         
     </body>
     
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,57 +7,110 @@
         </h2>
     <!--navigation.blade.phpからナビゲーションバーの項目を追加できる-->
     </x-slot>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        
-    </head>
+    <title>Laravel</title>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
     
-    <body>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">
-                        <h2 class=title>{{$post->title}}</h2>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
+    
+</head>
+
+<body class="antialiased">
+    
+    
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900">
+          <h2 class=title>{{$post->title}}</h2>
                         
-                        <h3 class=body>{{$post->body}}</h3>
+          <h3 class=body>{{$post->body}}</h3>
                         
-                        <h3 class=category>{{$post->category->name}}</h3>
-                        <div class="flex justify-end mt-4">
-                            <p class=user>投稿者：{{$post->user->name}}</p>
-                        </div>
+          <h3 class=category>{{$post->category->name}}</h3>
+          <div class="flex justify-end mt-4">
+            <p class=user>投稿者：{{$post->user->name}}</p>
+          </div>
                         
-                        <span>
-                                
-                            <!-- もし$favoriteがあれば＝ユーザーが「いいね」をしていたら -->
-                            @if($post->is_favorited($post))
-                            <!-- 「いいね」取消用ボタンを表示 -->
-                            	<a href="{{ route('unfavorite', $post) }}" class="btn btn-success btn-sm">
-                                	いいね取り消し
-                                	<span class="badge">
-                            			{{ $post->favorites->count() }}
-                            		</span>
-                            	</a>
-                            @else
-                            <!-- まだユーザーが「いいね」をしていなければ、「いいね」ボタンを表示 -->
-                            	<a href="{{ route('favorite', $post) }}" class="btn btn-secondary btn-sm">
-                            		いいね
-                            		<!-- 「いいね」の数を表示 -->
-                                	<span class="badge">
-                                		{{ $post->favorites->count() }}
-                                	</span>
-                                </a>
-                            @endif
-                        </span>
+          <span>  
+            <!-- もし$favoriteがあれば＝ユーザーが「いいね」をしていたら -->
+            @if($post->is_favorited($post))
+            <!-- 「いいね」取消用ボタンを表示 -->
+              <a href="{{ route('unfavorite', $post) }}" class="btn btn-success btn-sm">
+                いいね取り消し
+                <span class="badge">
+                  {{ $post->favorites->count() }}
+                </span>
+              </a>
+            @else
+            <!-- まだユーザーが「いいね」をしていなければ、「いいね」ボタンを表示 -->
+              <a href="{{ route('favorite', $post) }}" class="btn btn-secondary btn-sm">
+                いいね
+                <!-- 「いいね」の数を表示 -->
+                <span class="badge">
+                  {{ $post->favorites->count() }}
+                </span>
+              </a>
+            @endif
+          </span>
                         
-                        <a href="/">back</a>
-                    </div>
-                </div>
-            </div>
+          <a href="/">back</a>
+          
+          <div id="map" style="height:500px; width:800px;"></div>
+          
         </div>
-    </body>
-    </x-app-layout>
+      </div>
+    </div>
+  </div>
+
+  
+    <script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+        ({key: "APIKEY", v: "beta"});</script>
+    <script>
+            // Initialize and add the map
+      let map;
+      
+      async function initMap($post) {
+        // The location of Uluru
+        const position = { lat: {{$post->latitude}}, lng: {{$post->longitude}} };
+        // Request needed libraries.
+        //@ts-ignore
+        const { Map } = await google.maps.importLibrary("maps");
+        const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+      
+        // The map, centered at Uluru
+        map = new Map(document.getElementById("map"), {
+          zoom: 13,
+          center: position,
+          mapId: "MAP_ID",
+        });
+      
+        // The marker, positioned at Uluru
+        const marker = new AdvancedMarkerView({
+          map: map,
+          position: position,
+          title: "location",
+        });
+      }
+      
+      initMap();
+    </script>
+    
+    
+    
+</body>
+
+</x-app-layout>
 </html>
