49f5f3e41b2600c6529c6e91ef8fbe98a70f3fb0
poke0923 <orbis.9482@gmail.com>
2023-12-29 14:11:17 +0900

    chore:APIキーをenvファイルに移して表示できるようにした

--- config/services.php
+++ config/services.php
@@ -30,5 +30,9 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    
+    'googleMap' => [
+      'apikey' => env('GOOGLE_MAP_API_KEY'),
+    ],
 
 ];
