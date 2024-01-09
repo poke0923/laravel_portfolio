375881b5fb8c7cb9384400c9596c5ee373847e6e
poke0923 <orbis.9482@gmail.com>
2024-01-10 00:11:01 +0900

    fix:説明欄の文字数によるバリデーション、リダイレクト時の値の保持

--- app/Http/Requests/EditRequest.php
+++ app/Http/Requests/EditRequest.php
@@ -25,8 +25,21 @@ class EditRequest extends FormRequest
     {
         return [
             'post.title' => 'required|max:50',
+            'post.body' => 'max:200',
             
             //
         ];
     }
+    
+    public function messages()
+    {
+        return [
+            
+            'post.title.required' => 'タイトルは必ず入力してください。写真を更新する場合は再選択してください。',
+            'post.title.max' => 'タイトルは50文字以内で入力してください。写真を更新する場合は再選択してください。',
+            'post.body.max' => '写真説明は200文字以内で入力してください。写真を更新する場合は再選択してください。',
+            
+        ];
+    }
+    
 }
